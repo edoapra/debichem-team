9749d0954622e87872ac3db637ed9e1ff81e49f2
edoapra <edoardo.apra@gmail.com>
2018-08-15 11:57:56 -0700

    Initial Commit for Ubuntu Xenial

---
+++ nwchem/build_pkg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -f
+rm -rf nwchem-6.8.1
+NWCHEM_TARNAME=6.8.1-release.revision-v6.8-133-ge032219-src.2018-06-14
+if [ ! -f nwchem-"$NWCHEM_TARNAME".tar.bz2 ]; then
+curl -O -L https://github.com/nwchemgit/nwchem/releases/download/6.8.1-release/nwchem-"$NWCHEM_TARNAME".tar.bz2
+fi
+tar xjf nwchem-"$NWCHEM_TARNAME".tar.bz2
+cp -rp debian nwchem-6.8.1/.
+cd nwchem-6.8.1
+dpkg-buildpackage  -rfakeroot -us -uc 
