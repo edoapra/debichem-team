70352255135980353f63c33e62f46b05c77b177c
edoapra <edoardo.apra@gmail.com>
2018-08-15 18:03:43 -0700

    fixed tar name

--- nwchem/build_pkg.sh
+++ nwchem/build_pkg.sh
@@ -2,7 +2,7 @@
 rm -rf nwchem-6.8.1
 NWCHEM_ORGNAME=6.8.1+133+gitge032219.orig
 NWCHEM_TARNAME=6.8.1-release.revision-v6.8-133-ge032219-src.2018-06-14
-if [ ! -f nwchem-"$NWCHEM_ORGNAME".tar.bz2 ]; then
+if [ ! -f nwchem_"$NWCHEM_ORGNAME".tar.bz2 ]; then
 curl  -L https://github.com/nwchemgit/nwchem/releases/download/6.8.1-release/nwchem-"$NWCHEM_TARNAME".tar.bz2 -o nwchem_"$NWCHEM_ORGNAME".tar.bz2
 fi
 tar xjf nwchem_"$NWCHEM_ORGNAME".tar.bz2
