9d3809019b0b07f73c0c2b68ef71bccd8a1bc127
Edoardo Apra <edoardo.apra@gmail.com>
2022-11-24 22:40:07 -0800

    scripts update

--- nwchem/build_pkg.sh
+++ nwchem/build_pkg.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/env bash 
 rm -f v*.tar.*
 rm -rf nwchem*
 #uscan --download --download-current-version --verbose --destdir=. --force-download
 #tar xzf nwchem*.tar.gz
 wget -q https://github.com/nwchemgit/nwchem/tarball/release-7-2-0 -O nwchem_7.2.0.orig.tar.gz
-tar  --exclude=QA  --exclude=web --exclude=examples --exclude=doc  --exclude="nwxc*"  --exclude="chem-library-tests" --exclude=tce -xzf nwchem_7.2.0.orig.tar.gz
+tar  --exclude=web --exclude=examples --exclude=doc  --exclude="nwxc*"  --exclude="chem-library-tests"  -xzf nwchem_7.2.0.orig.tar.gz
 mv nwchemgit-nwchem-* nwchem-7.2.0
 cp -rp debian nwchem-*/.
 cd nwchem-*
---
+++ nwchem/req.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo apt install devscripts  debhelper dh-python
+sudo mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev
