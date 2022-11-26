0933dc7f23f03f7c3d39fb63a7606d3c04a52d22
edoapra <edoardo.apra@gmail.com>
2022-11-26 00:33:43 -0800

    cmake

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils pkg-config
-sudo apt-get -q=2 install -y mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev
-dpkg-query -L coreutils
-which timeout
-dpkg-query -S /usr/bin/timeout
+sudo apt-get -q=2 install -y mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev cmake
