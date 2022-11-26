2cd27f4f30a92ae1082d7cb31a245b22a19df2d6
edoapra <edoardo.apra@gmail.com>
2022-11-25 17:41:18 -0800

    coreutils

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo apt -q=2 install -y devscripts  debhelper dh-python moreutils
-sudo apt -q=2 install -y mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev
+sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils
+sudo apt-get -q=2 install -y mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev
