87c7751b9f029e8e549f3d94128d9c2b278d20a4
edoapra <edoardo.apra@gmail.com>
2022-11-30 17:40:27 -0800

    openmpi broken on ubuntu:focal/armv7

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils pkg-config
-sudo apt-get -q=2 install -y mpi-default-bin mpi-default-dev gfortran git make  patch python3-dev cmake
+arch5=$(uname -m|cut -c 1-5)
+mpipkg=" mpi-default-bin mpi-default-dev "
+if [[ "$arch5" == "armv7" ]]; then mpipkgs=" mpich libmpich-dev "; fi
+sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake "$mpipkgs"
