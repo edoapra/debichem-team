262bd66c18ba8493e17eb4bf45720756b79e24cd
edoapra <edoardo.apra@gmail.com>
2022-12-01 11:24:27 -0800

    fix mpipkgs

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -2,5 +2,5 @@
 sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils pkg-config
 arch5=$(uname -m|cut -c 1-5)
 mpipkgs=" mpi-default-bin mpi-default-dev "
-#if [[ "$arch5" == "armv7" ]]; then mpipkgs=" mpich libmpich-dev "; fi
-sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake "$mpipkgs"
+if [[ "$arch5" == "armv7" ]]; then mpipkgs=" mpich libmpich-dev "; fi
+sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake $mpipkgs
