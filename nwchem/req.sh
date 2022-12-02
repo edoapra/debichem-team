c17811d9ecde2b4df0a732b62da0bb3b89faaf77
edoapra <edoardo.apra@gmail.com>
2022-12-02 10:56:35 -0800

    removed mpich hack

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils pkg-config
 arch5=$(uname -m|cut -c 1-5)
-mpipkgs=" mpi-default-bin mpi-default-dev mpich libmpich-dev "
+mpipkgs=" mpi-default-bin mpi-default-dev "
 #if [[ "$arch5" == "armv7" ]]; then mpipkgs+=" mpich libmpich-dev "; fi
 sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake $mpipkgs
-sudo update-alternatives --set mpi /usr/bin/mpicc.mpich
-sudo update-alternatives --set mpirun  /usr/bin/mpirun.mpich
-mpif90 -show
-sed 's/openmpi/mpich/'  /usr/share/mpi-default-dev/debian_defaults > /tmp/new
-cat /tmp/new
-sudo mv /tmp/new /usr/share/mpi-default-dev/debian_defaults
