c3ba6e1da4be58e0d6d8f39df70e4cb0bdbff3ce
edoapra <edoardo.apra@gmail.com>
2022-12-17 18:55:04 -0800

    libxc

--- nwchem/req.sh
+++ nwchem/req.sh
@@ -4,4 +4,4 @@ sudo apt-get -q=2 install -y devscripts  debhelper dh-python coreutils pkg-confi
 arch5=$(uname -m|cut -c 1-5)
 mpipkgs=" mpi-default-bin mpi-default-dev "
 #if [[ "$arch5" == "armv7" ]]; then mpipkgs+=" mpich libmpich-dev "; fi
-sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake $mpipkgs
+sudo apt-get -q=2 install -y  gfortran git make  patch python3-dev cmake libxc-dev  $mpipkgs
