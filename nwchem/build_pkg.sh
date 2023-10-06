f87e4ebd4283bf292a94c044e56f67e21b54bc19
edoapra <edoardo.apra@gmail.com>
2023-10-05 17:03:43 -0700

    7.2.1 release

--- nwchem/build_pkg.sh
+++ nwchem/build_pkg.sh
@@ -3,10 +3,10 @@ rm -f v*.tar.*
 rm -rf nwchem*
 #uscan --download --download-current-version --verbose --destdir=. --force-download
 #tar xzf nwchem*.tar.gz
-wget -q https://github.com/nwchemgit/nwchem/tarball/release-7-2-0 -O nwchem_7.2.0.orig.tar.gz
+wget -q https://github.com/nwchemgit/nwchem/tarball/hotfix/release-7-2-0 -O nwchem_7.2.1.orig.tar.gz
 #tar  --exclude=web --exclude=examples --exclude=doc  --exclude="nwxc*"  --exclude="chem-library-tests"  -xzf nwchem_7.2.0.orig.tar.gz
-tar    -xzf nwchem_7.2.0.orig.tar.gz
-mv nwchemgit-nwchem-* nwchem-7.2.0
+tar    -xzf nwchem_7.2.1.orig.tar.gz
+mv nwchemgit-nwchem-* nwchem-7.2.1
 cp -rp debian nwchem-*/.
 cd nwchem-*
 # to fix running openmpi as root in docker
